c60e87295b117c9dd02a359fd6d5f268cb1ca1e3
stefanofilippini <filippini.stefano96@gmail.com>
2021-11-30 15:56:39 +0100

    Init Created header and form

---
+++ src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input v-model="Text" type="text">
+        <button @click="$emit('searchText', Text)">Click Me</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Form',
+    data() {
+        return {
+            Text: '',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+      <Form/>
+  </div>
+</template>
+
+<script>
+
+import Form from '@/components/Form';
+
+export default {
+    name: 'Header',
+
+    components: {
+        Form,
+    },
+
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped leng="scss">
+
+</style>
